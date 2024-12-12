e4a27d384b88fe6bf2b4d3c0d3bfafaafdb6467f
Harsh <hkpatel747@gmail.com>
2024-12-12 13:25:48 +0530

    Routes,Sign-in,Sign-up Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,70 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import Category from './components/Category';
 import Cards from './components/Cards';
+import LoginPage from './components/signup'; // Create this component
+import SignUpPage from './components/login'; // Create this component
+
 function App() {
-  
-
   return (
-    <>
-      
+    <Router>
       <Header />
-      <div className='ml-6 mr-6 font-poppins  bg-sky-200/50  h-[694px] flex   flex-col rounded-2xl'>
-        <div className='flex mt-64 flex-row justify-center items-center space-x-6'>
-        <div className='flex   text-center  font-bold text-6xl '>
-          Unleash 
+      <Routes>
+        {/* Home Page */}
+        <Route
+          path="/"
+          element={
+            <>
+              <div className='ml-6 mr-6 font-poppins bg-sky-200/50 h-[694px] flex flex-col rounded-2xl'>
+                <div className='flex mt-64 flex-row justify-center items-center space-x-6'>
+                  <div className='flex text-center font-bold text-6xl'>Unleash</div>
+                  <div>
+                    <img className='h-20 w-20 animate-spin-slow' src='/public/assets/blue.png' alt="spinning graphic" />
+                  </div>
+                  <div className='flex text-center font-bold text-6xl'>Creativity</div>
+                </div>
 
-          </div>
-            <div><img className='h-20 w-20 animate-spin-slow ' src='/public/assets/blue.png'/></div>
-          <div className='flex   text-center  font-bold text-6xl '>  
-            Creativity
-          </div>
-        </div>
+                <div className="flex flex-row justify-center item-center">
+                  <div className='text-center mt-2 font-extralight text-3xl'>
+                    Own Masterpieces Discover Art That Speaks to You
+                  </div>
+                </div>
 
-        <div className="flex flex-row justify-center item-center">
-          <div className='text-center mt-2 font-extralight text-3xl'> 
-            Own Masterpieces Discover Art That Speaks to You
-            
-          </div>
-          
-          
-          
-        </div>
+                {/* home page button */}
+                <div className='flex justify-center'>
+                  <button className='px-8 py-4 mt-32 rounded-full bg-gray-900 ring-2 ring-pink-400 text-white font-medium'>
+                    Explore products
+                  </button>
+                </div>
+                <div className='flex justify-center mt-10'>
+                  <img className="w-8 h-8 animate-bounce text-white opacity-20" src="/public/assets/down-arrow.svg" alt="down arrow" />
+                </div>
+              </div>
 
-        {/* home page button */}
-        <div className='flex justify-center'>
-        <button className='px-8 py-4 mt-32 rounded-full bg-gray-900 ring-2 ring-pink-400 text-white font-medium'>Explore products</button>
-        </div>
-        <div className='flex justify-center mt-10'>
-              <img className="w-8 h-8 animate-bounce text-white opacity-20"  src="/public/assets/down-arrow.svg"/>
-        </div>
-      </div>
+              {/* search section */}
+              <div className="flex ml-8 mt-10 space-x-4">
+                <SearchBar />
+                <Category />
+              </div>
+              <div className='flex flex-row'>
+                <Cards cardTitle="painting 1" productPrice="1299" />
+                <Cards />
+                <Cards />
+                <Cards />
+              </div>
+            </>
+          }
+        />
 
+        {/* Login Page */}
+        <Route path="/login" element={<LoginPage />} />
 
-      {/* search section */}
-      <div className="flex ml-8 mt-10 space-x-4">
-        <SearchBar/>
-        <Category/>
-      </div>
-      <div className='flex flex-row'>
-      <Cards cardTitle="painting 1" productPrice="1299"/>
-      <Cards/>
-      <Cards/>
-      <Cards/>
-      
-      
-      </div>
-      
-    </>
-  )
+        {/* Sign Up Page */}
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
@@ -6,24 +6,27 @@ function Header() {
       <div className="pl-10 pr-10 mt-8 mb-8 flex justify-between items-center font-poppins">
         <div className="flex flex-row items-center">
           <div>
-              <img className='h-10 w-10 items-center' src='/public/assets/pink.png'/>
+            <img className='h-10 w-10 items-center' src='/public/assets/pink.png' alt="Logo" />
           </div>
           <div className="font-bold text-3xl">
-              <h2>craftEra</h2>
+            <h2>craftEra</h2>
           </div>
         </div>
-        
-            <div className="flex flex-row text-md font-normal space-x-10 items-center align-middle">
-                <a href="#" className="">Home</a>
-                <a href="#" className=" ">About</a>
-                <a href="#" className=" ">Contact</a>
-                <div className="px-4 py-2 text-white rounded-full bg-gray-900">Signin</div>
-                <div className="px-4 py-2 text-white rounded-full bg-gray-900">Sign up</div>
-            </div>
-       
+
+        <div className="flex flex-row text-md font-normal space-x-10 items-center align-middle">
+          <Link to="/" className="">Home</Link>
+          <Link to="/about" className="">About</Link>
+          <Link to="/contact" className="">Contact</Link>
+          <Link to="/login">
+            <div className="px-4 py-2 text-white rounded-full bg-gray-900">Signin</div>
+          </Link>
+          <Link to="/signup">
+            <div className="px-4 py-2 text-white rounded-full bg-gray-900">Sign up</div>
+          </Link>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
