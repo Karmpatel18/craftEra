9554019f457b39493349538f120cac8f1da6f4b5
Karmpatel18 <karmpatel1203@gmail.com>
2024-12-12 23:15:32 +0530

    buying section

--- src/components/BuyCart.jsx
+++ src/components/BuyCart.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 
 function BuyCart() {
   return (
-    <div>
-      <div className='h-[540px] w-[640px] rounded-xl bg-slate-200/50 font-poppins shadow-2xl float-right mr-32'>
-        <div className='flex p-8 justify-between items-center'>
-        <div className='text-2xl text-gray-900 font-semibold'>pating for sale</div>
-        <div className='text-3xl text-gray-900 bg-white  p-2 rounded-lg'>1200</div>
+    <div className='ml-36 flex flex-row w-3/4 bg-slate-200/50 h-[680px] rounded-2xl'>
+      {/* left section */}
+      <div className='flex-1 flex-col'>
+        <div className='bg-black h-3/5 w-auto m-8 rounded-xl content-center '>
+          <img className='object-cover' src='https://images.pexels.com/photos/1509534/pexels-photo-1509534.jpeg?auto=compress&cs=tinysrgb&w=800='/>
         </div>
-        <div className='px-8 '>
-            description is very simple and simple text with color    and background color and background
+        <div className='flex w-auto h-auto m-8 justify-between'>
+          <div className='bg-black  rounded-lg h-24 w-36 place-content-stretch'>
+            <img className='object-fill h-24'src='https://images.pexels.com/photos/5788376/pexels-photo-5788376.jpeg?auto=compress&cs=tinysrgb&w=800'/>
+          </div>
+          <div className='bg-black  rounded-lg h-24 w-36'></div>
+          <div className='bg-black  rounded-lg h-24 w-36'></div>
         </div>
       </div>
+      {/* right section */}
+      <div className='flex-1'></div>
     </div>
   )
 }
