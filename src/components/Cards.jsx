288cb129e2604800feb4bbe9501a8ddf6f60ab0b
Karmpatel18 <karmpatel1203@gmail.com>
2024-12-18 11:34:04 +0530

    card ui c

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,16 +4,16 @@ function Cards(props) {
   
   return (
     <div className='font-poppins p-8'>
-      <div className='bg-slate-200/50 h-full w-64 rounded-xl   hover:shadow-xl duration-500'>
-        <div className='p-2'>
-          <img className='rounded-[6px] max-fit'src="https://images.pexels.com/photos/1509534/pexels-photo-1509534.jpeg?auto=compress&cs=tinysrgb&w=800="/>
+      <div className='bg-neutral-100 h-72 w-64 rounded-sm hover:bg-neutral-300/50   duration-500'>
+        <div className='p-2 h-[100%] w-[100%] object-cover '>
+          <img className='rounded-[2px] h-[100%] w-[100%] object-cover 'src="https://images.pexels.com/photos/4272616/pexels-photo-4272616.jpeg?auto=compress&cs=tinysrgb&w=800"/>
         </div>
-        <div className='px-4 pt-2 font-medium text-xl'>{props.cardTitle}</div>
-        <div className='px-4'>{props.productPrice}</div>
-        <div className='flex flex-row space-x-2 p-2'>
-          <button className='bg-gray-800 text-white rounded-lg w-full font-medium px-4 py-2 hover:bg-gray-900 duration-500'>Buy now</button>
-          <div className='font-light text-4xl bg-white px-4 py-2 rounded-lg cursor-pointer  hover:bg-pink-500  hover:text-white duration-150' >+</div>
-        </div>
+
+        
+      </div>
+      <div className="ml-2">
+      <div className=' pt-2 font-normal tracking-normal text-sm text-neutral-600'>{props.cardTitle}</div>
+      <div className='font-medium  text-md tracking-widest text-neutral-900'>RS. {props.productPrice}</div>
       </div>
     </div>
   )
@@ -21,3 +21,7 @@ function Cards(props) {
 
 export default Cards
 
+{/* <div className='flex flex-row space-x-2 p-2'>
+          <button className='bg-gray-800 text-white rounded-lg w-full font-medium px-4 py-2 hover:bg-gray-900 duration-500'>Buy now</button>
+          <div className='font-light text-4xl bg-white px-4 py-2 rounded-lg cursor-pointer  hover:bg-pink-500  hover:text-white duration-150' >+</div>
+        </div> */}
