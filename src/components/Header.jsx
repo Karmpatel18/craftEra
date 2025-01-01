ca557a93fdd0a0323e9914a6bbdb03df20f8fa68
Karmpatel18 <karmpatel1203@gmail.com>
2025-01-01 14:30:06 +0530

    nav button change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { NavLink } from "react-router-dom";
+import CircumIcon from "@klarr-agency/circum-icons-react"; // React
 
 function Header() {
   const[isLoggedIn , setIsLoggedIn] = useState(true);
@@ -27,26 +28,26 @@ function Header() {
           className={({ isActive }) =>
             isActive
               ? "text-neutral-50  bg-neutral-800 px-4 py-2 duration-200 rounded-lg "
-              : "text-gray-900 hover:text-gray-700 bg-white px-4 py-2 duration-200 rounded-lg "
+              : "text-gray-900 hover:bg-neutral-200 bg-white px-4 py-2 duration-200 rounded-lg "
           }
           >Home</NavLink>
           
           <NavLink to="/about" className={({ isActive }) =>
               isActive
                 ? "text-neutral-50  bg-neutral-800 px-4 py-2 duration-200 rounded-lg "
-                : "text-gray-900 hover:text-gray-700 bg-white px-4 py-2 duration-200 rounded-lg "
+                : "text-gray-900 hover:bg-neutral-200 bg-white px-4 py-2 duration-200 rounded-lg "
             }>About</NavLink>
           <NavLink to="/contact" className={({ isActive }) =>
               isActive
                 ? "text-neutral-50  bg-neutral-800 px-4 py-2 duration-200 rounded-lg "
-                : "text-gray-900 hover:text-gray-700 bg-white px-4 py-2 duration-200 rounded-lg "
+                : "text-gray-900 hover:bg-neutral-200 bg-white px-4 py-2 duration-200 rounded-lg "
             }>Contact</NavLink>
           
           
           {/* <div onClick={onClick} className="h-8 w-8 text-white rounded-full bg-gray-900"></div> */}
           {isLoggedIn ? (<div className="flex " onClick={onClick}> 
               <Link to="/login">
-                { isLoggedIn &&<div className="px-4 py-2 text-gray-800  ring-[2px] ring-inset ring-neutral-700 rounded-lg bg-neutral-50 text-ellipsis font-normal">Get started</div>}
+                { isLoggedIn &&<div className='flex ring-[2px] ring-inset ring-neutral-700 rounded-lg bg-neutral-50 gap-2 px-4 py-2 items-center text-center'><div className=" text-gray-800   text-ellipsis font-normal">Get started</div><div className="flex items-center h-6 w-6"><CircumIcon name="login"/></div></div>}
               </Link>
               {/* <Link to="/signup">
                 { isLoggedIn && <div className="px-4 py-2 text-white rounded-lg bg-gray-900">Sign up</div>}
