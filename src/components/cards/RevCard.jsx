94577dc19e7040b730d0196cbf1dd247de883766
Karmpatel18 <karmpatel1203@gmail.com>
2025-02-28 21:56:51 +0530

    order page ui change

--- src/components/cards/RevCard.jsx
+++ src/components/cards/RevCard.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
 
-const RevCard = (props) => {
+const RevCard = (Props) => {
     return (
-        <div className='w-[320px] h-[125px] mx-1.5 my-1 block border-[1px] shadow-md rounded-sm border-neutral-200'>
+        <div className='w-[320px] h-[125px] mx-1.5 my-1 block transition-all duration-300  border-[1px] hover:shadow-md rounded-sm border-neutral-200'>
             <div className="Info  w-fit  my-5.5 mb-0 flex gap-2 items-center justify-start  px-3.5">
-            <h1 className='font-light'>{props.title}</h1>
+            <h1 className='font-normal tracking-tighter'>{Props.title}</h1>
                 <div className="InfoBtn">
                     <a href="#">
                          <svg width="15" height="15" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#000000">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M8.568 1.031A6.8 6.8 0 0 1 12.76 3.05a7.06 7.06 0 0 1 .46 9.39 6.85 6.85 0 0 1-8.58 1.74 7 7 0 0 1-3.12-3.5 7.12 7.12 0 0 1-.23-4.71 7 7 0 0 1 2.77-3.79 6.8 6.8 0 0 1 4.508-1.149zM9.04 13.88a5.89 5.89 0 0 0 3.41-2.07 6.07 6.07 0 0 0-.4-8.06 5.82 5.82 0 0 0-7.43-.74 6.06 6.06 0 0 0 .5 10.29 5.81 5.81 0 0 0 3.92.58zM7.375 6h1.25V5h-1.25v1zm1.25 1v4h-1.25V7h1.25z" />
+                            <path  d="M8.568 1.031A6.8 6.8 0 0 1 12.76 3.05a7.06 7.06 0 0 1 .46 9.39 6.85 6.85 0 0 1-8.58 1.74 7 7 0 0 1-3.12-3.5 7.12 7.12 0 0 1-.23-4.71 7 7 0 0 1 2.77-3.79 6.8 6.8 0 0 1 4.508-1.149zM9.04 13.88a5.89 5.89 0 0 0 3.41-2.07 6.07 6.07 0 0 0-.4-8.06 5.82 5.82 0 0 0-7.43-.74 6.06 6.06 0 0 0 .5 10.29 5.81 5.81 0 0 0 3.92.58zM7.375 6h1.25V5h-1.25v1zm1.25 1v4h-1.25V7h1.25z" />
                         </svg>
                     </a>
                 </div>
             </div>
             <div className="amount flex justify-start mx-2.5  p-2">
-                <h1 className='text-3xl'>{props.sign}{props.amount}</h1>
+                <h1 className='text-3xl font-medium tracking-tight'>{Props.sign}{Props.amount}</h1>
             </div>
         </div>
     )
--- src/components/userProfile/OrdersPage.jsx
+++ src/components/userProfile/OrdersPage.jsx
@@ -1,25 +1,27 @@
+
 import RevCard from "../cards/RevCard"
 function OrdersPage() {
   return (
-    <div className="block h-full w-full ">
+    <div className="block h-full w-full bg-white">
       <div className="py-3 px-2  flex justify-between border-b-[1px]  border-neutral-200 ">
-        <h1 className="text-xl">Overview</h1>
-        <div className="calender flex shadow-lg border-[1px] px-3 border-neutral-200 rounded-md gap-4 items-center  w-fit">
+        <div className="text-xl  flex items-center px-2 font-medium tracking-tighter">Overview</div>
+        <div className="calender flex  border-[1px] px-3 py-2 border-neutral-200 rounded-md gap-4 items-center  w-fit">
           <div className="img  cursor-pointer w-fit flex items-center justify-center ">
             <svg height="24" width="24" viewBox="0 0 24 24" fill="whit" xmlns="http://www.w3.org/2000/svg">
               <path d="M8 12C7.44772 12 7 12.4477 7 13C7 13.5523 7.44772 14 8 14H16C16.5523 14 17 13.5523 17 13C17 12.4477 16.5523 12 16 12H8Z" fill="black" />
               <path d="M7 17C7 16.4477 7.44772 16 8 16H12C12.5523 16 13 16.4477 13 17C13 17.5523 12.5523 18 12 18H8C7.44772 18 7 17.5523 7 17Z" fill="black" />
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M7 2C7.55228 2 8 2.44772 8 3V4H16V3C16 2.44772 16.4477 2 17 2C17.5523 2 18 2.44772 18 3V4.10002C20.2822 4.56329 22 6.58104 22 9V17C22 19.7614 19.7614 22 17 22H7C4.23858 22 2 19.7614 2 17V9C2 6.58104 3.71776 4.56329 6 4.10002V3C6 2.44772 6.44772 2 7 2ZM20 10H4V17C4 18.6569 5.34315 20 7 20H17C18.6569 20 20 18.6569 20 17V10Z" fill="black" />
+              <path  d="M7 2C7.55228 2 8 2.44772 8 3V4H16V3C16 2.44772 16.4477 2 17 2C17.5523 2 18 2.44772 18 3V4.10002C20.2822 4.56329 22 6.58104 22 9V17C22 19.7614 19.7614 22 17 22H7C4.23858 22 2 19.7614 2 17V9C2 6.58104 3.71776 4.56329 6 4.10002V3C6 2.44772 6.44772 2 7 2ZM20 10H4V17C4 18.6569 5.34315 20 7 20H17C18.6569 20 20 18.6569 20 17V10Z" fill="black" />
             </svg>
           </div>
           <div className="Months">
             Feb-1 to Feb-28,2025
           </div>
+         
         </div>
       </div>
       <div className="content w-full ">
-      <h1 className="text-xl px-3 my-2.5 mx-1.5">Performance</h1>
-      <div className="RevCards w-full  grid justify-evenly grid-cols-3 gap-6 px-2  ">
+      <h1 className="text-xl px-4 py-3 font-medium tracking-tighter">Performance</h1>
+      <div className="RevCards w-full justify-evenly flex flex-wrap  py-2  ">
         <RevCard title="Total Sales" sign="$" amount="25,000"/>
         <RevCard title="Net Sales" sign="$" amount="10,000"/>
         <RevCard title="Total Tax" sign="$" amount="1500"/>
